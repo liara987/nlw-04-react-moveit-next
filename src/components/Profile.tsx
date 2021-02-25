828497ffc2d73fdee0cf0161c2d0a4c02be5c8e3
Liara Pereira Duarte <lipereira@uolinc.com>
2021-02-25 02:12:17 -0300

    Aulas 1, 2 e 3

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/liara987.png" alt="Liara Programadora" />
+      <div>
+        <strong>Liara Programadora</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
